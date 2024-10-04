3ac7ff1e40161eb7bc8433267577702c25a229e4
marcgonzaleztarrio <marcgonzaleztarrio@gmail.com>
2024-10-04 13:30:15 +0200

    update responsive and images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            keyframes: {
+                bounceSmall: {
+                    "0%, 100%": {
+                        transform: "translateY(-10%)",
+                        "animation-timing-function":
+                            "cubic-bezier(0.8, 0, 1, 1)",
+                    },
+                    "50%": {
+                        transform: "translateY(0)",
+                        "animation-timing-function":
+                            "cubic-bezier(0, 0, 0.2, 1)",
+                    },
+                },
+            },
+            animation: {
+                "bounce-small": "bounceSmall 1s infinite",
+            },
+        },
+    },
+    plugins: [],
+};
